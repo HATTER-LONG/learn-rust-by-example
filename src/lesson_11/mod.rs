e3a6a67cff8e064974afd13251aec73ede86ddad
caolei <caolei6767@gmail.com>
2025-01-06 17:20:18 +0800

    添加新类型示例和包含特征示例到第11课

--- src/lesson_11/mod.rs
+++ src/lesson_11/mod.rs
@@ -8,6 +8,8 @@ pub fn lesson_11_main() {
     bound_example();
     multi_bound_example();
     where_bound_example();
+    newtype_example();
+    contains_trait_example();
     info!("Lesson 11: end");
 }
 
@@ -30,6 +32,7 @@ fn template_example() {
     generic(SGen('c'));
 }
 
+#[allow(dead_code)]
 fn template_impl_example() {
     struct S;
     struct GenericVal<T>(T);
@@ -211,4 +214,78 @@ fn where_bound_example() {
 
     let vec = vec![1, 2, 3];
     vec.print_in_option();
+}
+
+fn newtype_example() {
+    struct Years(i64);
+    struct Days(i64);
+
+    impl Years {
+        pub fn to_days(&self) -> Days {
+            Days(self.0 * 365)
+        }
+    }
+
+    impl Days {
+        pub fn to_years(&self) -> Years {
+            Years(self.0 / 365)
+        }
+    }
+
+    fn old_enough(age: &Years) -> bool {
+        age.0 >= 18
+    }
+
+    let age = Years(5);
+    let age_days = age.to_days();
+
+    println!("Old enough: {}", old_enough(&age));
+    println!("Old enough: {}", old_enough(&age_days.to_years()));
+}
+
+fn contains_trait_example() {
+    struct Container(i32, i32);
+
+    trait Contains<A, B> {
+        fn contains(&self, _: &A, _: &B) -> bool;
+        fn first(&self) -> i32;
+        fn last(&self) -> i32;
+    }
+
+    impl Contains<i32, i32> for Container {
+        fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+            (&self.0 == number_1) && (&self.1 == number_2)
+        }
+
+        fn first(&self) -> i32 {
+            self.0
+        }
+
+        fn last(&self) -> i32 {
+            self.1
+        }
+    }
+
+    fn difference<A, B, C>(container: &C) -> i32
+    where
+        C: Contains<A, B>,
+    {
+        container.last() - container.first()
+    }
+
+    let number_1 = 3;
+    let number_2 = 10;
+
+    let container = Container(number_1, number_2);
+
+    println!(
+        "Does container contain {} and {}: {}",
+        &number_1,
+        &number_2,
+        container.contains(&number_1, &number_2)
+    );
+    println!("First number: {}", container.first());
+    println!("Last number: {}", container.last());
+
+    println!("The difference is: {}", difference(&container));
 }
