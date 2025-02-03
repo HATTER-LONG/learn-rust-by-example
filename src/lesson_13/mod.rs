bbbcadbaabbd19dab9012abf36139fe1cdcf2f04
Layton <caolei6767@gmail.com>
2025-02-03 14:38:41 +0800

    添加第13课，包含新的特征 UsernameWidget 和 AgeWidget 以及相应的实现示例

--- src/lesson_13/mod.rs
+++ src/lesson_13/mod.rs
@@ -279,25 +279,38 @@ fn clone_example() {
 }
 
 fn base_trait_example() {
-    trait Person {
-        fn name(&self) -> String;
+    trait UsernameWidget {
+        fn get(&self) -> String;
     }
 
-    trait Student: Person {
-        fn university(&self) -> String;
+    trait AgeWidget {
+        fn get(&self) -> u8;
     }
 
-    trait Programmer {
-        fn fav_language(&self) -> String;
+    struct Form {
+        username: String,
+        age: u8,
     }
 
-    trait CompSciStudent: Programmer + Student {
-        fn git_username(&self) -> String;
+    impl UsernameWidget for Form {
+        fn get(&self) -> String {
+            self.username.clone()
+        }
     }
 
-    struct ComputerScienceStudent {
-        name: String,
-        university: String,
-        git_username: String,
+    impl AgeWidget for Form {
+        fn get(&self) -> u8 {
+            self.age
+        }
     }
+
+    let form = Form {
+        username: "rustacean".to_owned(),
+        age: 28,
+    };
+
+    let username = <Form as UsernameWidget>::get(&form);
+    assert_eq!("rustacean".to_owned(), username);
+    let age = <Form as AgeWidget>::get(&form);
+    assert_eq!(28, age);
 }
