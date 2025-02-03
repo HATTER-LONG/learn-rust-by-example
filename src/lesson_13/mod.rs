06f71c65696634c9a6ad65790bfc04813742c7c3
Layton <caolei6767@gmail.com>
2025-02-03 10:30:24 +0800

    添加第13课，包含运算符重载、Drop 特征、迭代器、克隆示例和基础特征示例

--- src/lesson_13/mod.rs
+++ src/lesson_13/mod.rs
@@ -5,6 +5,11 @@ pub fn lesson_13_main() {
     trait_base_example();
     derive_example();
     dyn_trait_example();
+    overried_example();
+    drop_trait_example();
+    iterator_example();
+    clone_example();
+    base_trait_example();
     info!("Lesson 13: end");
 }
 
@@ -131,4 +136,168 @@ fn dyn_trait_example() {
         "You've randomly chosen an animal, and it says {}",
         animal.noise()
     );
+}
+
+fn overried_example() {
+    use std::ops;
+
+    struct Foo;
+    struct Bar;
+
+    #[derive(Debug)]
+    struct FooBar;
+
+    #[derive(Debug)]
+    struct BarFoo;
+
+    impl ops::Add<Bar> for Foo {
+        type Output = FooBar;
+
+        fn add(self, _rhs: Bar) -> FooBar {
+            println!("> Foo.add(Bar) was called");
+            FooBar
+        }
+    }
+
+    impl ops::Add<Foo> for Bar {
+        type Output = BarFoo;
+
+        fn add(self, _rhs: Foo) -> BarFoo {
+            println!("> Bar.add(Foo) was called");
+            BarFoo
+        }
+    }
+
+    println!("Foo + Bar = {:?}", Foo + Bar);
+    println!("Bar + Foo = {:?}", Bar + Foo);
+}
+
+fn drop_trait_example() {
+    struct Droppable {
+        name: &'static str,
+    }
+
+    impl Drop for Droppable {
+        fn drop(&mut self) {
+            println!("> Dropping {}", self.name);
+        }
+    }
+
+    let _a = Droppable { name: "a" };
+    {
+        let _b = Droppable { name: "b" };
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+            println!("Exiting inner scope");
+        }
+        println!("Just exited inner scope");
+    }
+}
+
+fn iterator_example() {
+    struct Fibonacci {
+        curr: u32,
+        next: u32,
+    }
+
+    impl Iterator for Fibonacci {
+        type Item = u32;
+
+        fn next(&mut self) -> Option<u32> {
+            let new_next = self.curr + self.next;
+            self.curr = self.next;
+            self.next = new_next;
+            Some(self.curr)
+        }
+    }
+
+    fn fibonacci() -> Fibonacci {
+        Fibonacci { curr: 1, next: 1 }
+    }
+
+    // `0..3` 是一个 `Iterator`，会产生：0、1 和 2。
+    let mut sequence = 0..3;
+
+    println!("Four consecutive `next` calls on 0..3");
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+
+    // `for` 遍历 `Iterator` 直到返回 `None`，
+    // 并且每个 `Some` 值都被解包（unwrap），然后绑定给一个变量（这里是 `i`）。
+    println!("Iterate through 0..3 using `for`");
+    for i in 0..3 {
+        println!("> {}", i);
+    }
+
+    // `take(n)` 方法提取 `Iterator` 的前 `n` 项。
+    println!("The first four terms of the Fibonacci sequence are: ");
+    for i in fibonacci().take(4) {
+        println!("> {}", i);
+    }
+
+    // `skip(n)` 方法移除前 `n` 项，从而缩短了 `Iterator` 。
+    println!("The next four terms of the Fibonacci sequence are: ");
+    for i in fibonacci().skip(4).take(4) {
+        println!("> {}", i);
+    }
+
+    let array = [1u32, 3, 3, 7];
+
+    // `iter` 方法对数组/slice 产生一个 `Iterator`。
+    println!("Iterate the following array {:?}", &array);
+    for i in array.iter() {
+        println!("> {}", i);
+    }
+}
+
+fn clone_example() {
+    #[derive(Debug, Clone, Copy)]
+    struct Nil;
+
+    #[derive(Clone, Debug)]
+    struct Pair(Box<i32>, Box<i32>);
+
+    let nil = Nil;
+    let copied_nil = nil;
+
+    println!("original: {:?}", nil);
+    println!("copy: {:?}", copied_nil);
+
+    let pair = Pair(Box::new(1), Box::new(2));
+    println!("original: {:?}", pair);
+
+    let moved_pair = pair;
+    println!("copy: {:?}", moved_pair);
+
+    let cloned_pair = moved_pair.clone();
+    drop(moved_pair);
+
+    println!("clone: {:?}", cloned_pair);
+}
+
+fn base_trait_example() {
+    trait Person {
+        fn name(&self) -> String;
+    }
+
+    trait Student: Person {
+        fn university(&self) -> String;
+    }
+
+    trait Programmer {
+        fn fav_language(&self) -> String;
+    }
+
+    trait CompSciStudent: Programmer + Student {
+        fn git_username(&self) -> String;
+    }
+
+    struct ComputerScienceStudent {
+        name: String,
+        university: String,
+        git_username: String,
+    }
 }
